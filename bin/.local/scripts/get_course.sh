8867c3de7e7dec189bb45a6c2f4cd04f24901305
Emil Behnke <eb531@web.de>
2023-11-05 15:15:54 +0100

    Updated scripts

--- bin/.local/scripts/get_course.sh
+++ bin/.local/scripts/get_course.sh
@@ -2,5 +2,4 @@
 course=$(cat /home/emil/.local/scripts/courses.txt | rofi -dmenu -no-custom -p ">" -l 8 )
 course_id=$(echo $course | tr " " "\n" | tail -n1)
 
-librewolf --new-tab "https://166250.logineonrw-lms.de/course/view.php?id=${course_id}"
-
+firefox --new-tab "https://ilias.studium.kit.edu/goto.php?target=${course_id}&client_id=produktiv"
